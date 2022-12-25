23c63a979fdbd2cf0ff5dcfa1efa36fc0fa3f313
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-12-26 01:27:56 +0530

    Drag and drop enhanced

--- src/components/right/Right.js
+++ src/components/right/Right.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { Col, ListGroup } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Draggable from "react-draggable";
 import { DragDrop } from "../../contextApi/dragDropContext";
 
 export default function Right() {
@@ -17,36 +17,9 @@ export default function Right() {
     setTest,
     blankComponent,
     setBlankComponent,
-    list,
     setList,
+    list,
   } = useContext(DragDrop);
-  const button = [
-    { id: "11", name: "Bar" },
-    { id: "12", name: "Chart" },
-    { id: "13", name: "Title" },
-    { id: "14", name: "Table" },
-    { id: "15", name: "Divider" },
-  ];
-  const [stateData, updateStateData] = useState(button);
-
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
-
-    const items = Array.from(stateData);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    updateStateData(items);
-    console.log(result);
-    setList([
-      ...list,
-      {
-        id: `${result.draggableId}${Math.floor(Math.random() * 90)}`,
-        name: result.draggableId,
-      },
-    ]);
-  }
-
   return (
     <Col>
       <div
@@ -64,42 +37,134 @@ export default function Right() {
             <Button variant="secondary">Charts</Button>
           </ButtonGroup>
           <div className="d-grid gap-2">
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-              <Droppable droppableId="button">
-                {(provided) => (
-                  <ListGroup
-                    className="button"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {stateData.map((item, index) => {
-                      return (
-                        <Draggable
-                          key={item.name}
-                          draggableId={item.name}
-                          index={index}
-                        >
-                          {(provided) => (
-                            <ListGroup.Item
-                              variant="outline-success"
-                              size="md"
-                              style={{ textAlign: "left" }}
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                            >
-                              {item.name}
-                            </ListGroup.Item>
-                          )}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
-                  </ListGroup>
-                )}
-              </Droppable>
-            </DragDropContext>
+            {!bar && (
+              <Draggable
+                axis="x"
+                onStop={() => {
+                  setList([
+                    ...list,
+                    { id: `Bar${Math.floor(Math.random() * 90)}`, name: "Bar" },
+                  ]);
+                  setBar(true);
+                }}
+              >
+                <Button
+                  variant="outline-success"
+                  size="md"
+                  style={{ textAlign: "left" }}
+                >
+                  Bar Graph
+                </Button>
+              </Draggable>
+            )}
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Row
+            </Button>
+            {!chart && (
+              <Draggable
+                onStop={() => {
+                  setList([
+                    ...list,
+                    {
+                      id: `Chart${Math.floor(Math.random() * 90)}`,
+                      name: "Chart",
+                    },
+                  ]);
+                  setChart(true);
+                }}
+              >
+                <Button
+                  variant="outline-success"
+                  size="md"
+                  style={{ textAlign: "left" }}
+                >
+                  Chart Graph
+                </Button>
+              </Draggable>
+            )}
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Header
+            </Button>
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Markdown
+            </Button>
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Divider
+            </Button>
+            {!table && (
+              <Draggable
+                onStop={() => {
+                  setList([
+                    ...list,
+                    {
+                      id: `Table${Math.floor(Math.random() * 90)}`,
+                      name: "Table",
+                    },
+                  ]);
+                  setTable(true);
+                }}
+              >
+                <Button
+                  variant="outline-success"
+                  size="md"
+                  style={{ textAlign: "left" }}
+                >
+                  Table
+                </Button>
+              </Draggable>
+            )}
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Process Diagram
+            </Button>
+            <Button
+              variant="outline-success"
+              size="md"
+              style={{ textAlign: "left" }}
+            >
+              Trigger Flow
+            </Button>
+            {!test && (
+              <Draggable
+                onStop={() => {
+                  setList([
+                    ...list,
+                    {
+                      id: `Title${Math.floor(Math.random() * 90)}`,
+                      name: "Title",
+                    },
+                  ]);
+                  setTest(true);
+                }}
+              >
+                <Button
+                  variant="outline-success"
+                  size="md"
+                  style={{ textAlign: "left" }}
+                >
+                  Title
+                </Button>
+              </Draggable>
+            )}
           </div>
         </div>
       </div>
